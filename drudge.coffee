4695745a98f59de26e2402141e5585867a16c05a
Matt White <matt@thermalcreative.com>
2012-07-07 22:45:39 -0600

    Version 0.1

--- drudge.coffee
+++ drudge.coffee
@@ -1,36 +1,61 @@
 checkRequirements = ->
   try
-    return window.localStorage && window['localStorage'] != null;
+    return window.localStorage && window['localStorage'] != null
   catch error
-    return false;
+    return false
+  
+  return false if window.location.href.indexOf "drudgereport.com" >= 0
   true;
 
 loadLinks = ->
   if data = localStorage.getItem("drudge.js.links")
-    data = JSON.parse data;
+    data = JSON.parse data
   else
-    data = [];
+    data = {}
 
-  found = [];
-  for index, anchor of document.getElementsByTagName "a"
-    url = anchor.getAttribute "href";
+  found = []
+  for anchor, index in document.getElementsByTagName "a"
+    url = anchor.getAttribute "href"
+    continue if url.length == 0
+    continue if url == "http://www.drudgereport.com/"
+
     found.push url;
-    
+
     if data[url]
       data[url].views++;
     else
-      data[url] = views: 1, text: anchor.text;
-    anchor.setAttribute 'style', "color: green;" if data[url].views == 1;
-    anchor.setAttribute 'style', "color: blue;" if data[url].views > 1;
- 
-  console.log "fired";
+      data[url] = views: 1, text: anchor.text
 
-  for url, record of data
+    if data[url].views == 1
+      anchor.setAttribute 'style', "color: green;"
+    else
+      opacity = (data[url].views / 20) + 0.5 if data[url].views < 10
+      anchor.setAttribute 'style', "opacity: " + opacity
+
+  for own url, record of data
     delete data[url] unless found.indexOf(url) > 0
 
   localStorage.setItem "drudge.js.links", JSON.stringify(data)
+  document.body.setAttribute "class", document.body.getAttribute("class") + "drudge_js_ran"
+
+loadUI = ->
+  navBar = document.createElement 'div'
+  navBar.setAttribute "style", "position:absolute;top:10px;right:10px;border:1px solid #000;box-shadow:1px 1px 3px #000;border-radius: 3px;padding:3px;background:#000"
+  
+  resetButton = document.createElement 'a'
+  resetButton.innerHTML = "Reset"
+  resetButton.setAttribute "style", "font-family:sans-serif;color:#fff;text-decoration:none;cursor:pointer;"
+  navBar.appendChild resetButton
+
+  document.body.appendChild navBar
+
+  resetButton.onclick = ->
+    localStorage.removeItem("drudge.js.links")
+    loadLinks()
 
 if checkRequirements()
-  loadLinks();
+  if document.body.getAttribute("class") == null || document.body.getAttribute("class").indexOf("drudge_js_ran") < 0
+    loadLinks();
+    loadUI();
 else
-  alert "your browser sucks, go away."
+  alert "You're not at drudgereport.com, or your browser can't support this tool. Supported browsers are IE9, Chrome, Safari, or Firefox"
--- drudge.js
+++ drudge.js
@@ -1,5 +1,6 @@
 (function() {
-  var checkRequirements, loadLinks;
+  var checkRequirements, loadLinks, loadUI,
+    __hasProp = Object.prototype.hasOwnProperty;
 
   checkRequirements = function() {
     try {
@@ -7,21 +8,24 @@
     } catch (error) {
       return false;
     }
+    if (window.location.href.indexOf("drudgereport.com" >= 0)) return false;
     return true;
   };
 
   loadLinks = function() {
-    var anchor, data, found, index, record, url, _ref;
+    var anchor, data, found, index, opacity, record, url, _len, _ref;
     if (data = localStorage.getItem("drudge.js.links")) {
       data = JSON.parse(data);
     } else {
-      data = [];
+      data = {};
     }
     found = [];
     _ref = document.getElementsByTagName("a");
-    for (index in _ref) {
+    for (index = 0, _len = _ref.length; index < _len; index++) {
       anchor = _ref[index];
       url = anchor.getAttribute("href");
+      if (url.length === 0) continue;
+      if (url === "http://www.drudgereport.com/") continue;
       found.push(url);
       if (data[url]) {
         data[url].views++;
@@ -31,21 +35,44 @@
           text: anchor.text
         };
       }
-      if (data[url].views === 1) anchor.setAttribute('style', "color: green;");
-      if (data[url].views > 1) anchor.setAttribute('style', "color: blue;");
+      if (data[url].views === 1) {
+        anchor.setAttribute('style', "color: green;");
+      } else {
+        if (data[url].views < 10) opacity = (data[url].views / 20) + 0.5;
+        anchor.setAttribute('style', "opacity: " + opacity);
+      }
     }
-    console.log("fired");
     for (url in data) {
+      if (!__hasProp.call(data, url)) continue;
       record = data[url];
       if (!(found.indexOf(url) > 0)) delete data[url];
     }
-    return localStorage.setItem("drudge.js.links", JSON.stringify(data));
+    localStorage.setItem("drudge.js.links", JSON.stringify(data));
+    return document.body.setAttribute("class", document.body.getAttribute("class") + "drudge_js_ran");
+  };
+
+  loadUI = function() {
+    var navBar, resetButton;
+    navBar = document.createElement('div');
+    navBar.setAttribute("style", "position:absolute;top:10px;right:10px;border:1px solid #000;box-shadow:1px 1px 3px #000;border-radius: 3px;padding:3px;background:#000");
+    resetButton = document.createElement('a');
+    resetButton.innerHTML = "Reset";
+    resetButton.setAttribute("style", "font-family:sans-serif;color:#fff;text-decoration:none;cursor:pointer;");
+    navBar.appendChild(resetButton);
+    document.body.appendChild(navBar);
+    return resetButton.onclick = function() {
+      localStorage.removeItem("drudge.js.links");
+      return loadLinks();
+    };
   };
 
   if (checkRequirements()) {
-    loadLinks();
+    if (document.body.getAttribute("class") === null || document.body.getAttribute("class").indexOf("drudge_js_ran") < 0) {
+      loadLinks();
+      loadUI();
+    }
   } else {
-    alert("your browser sucks, go away.");
+    alert("You're not at drudgereport.com, or your browser can't support this tool. Supported browsers are IE9, Chrome, Safari, or Firefox");
   }
 
 }).call(this);
